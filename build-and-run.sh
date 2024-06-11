3ed5de17f0a513247b245f482c796c872633ff98
Zin Yosrim <zinyosrim@gmail.com>
2024-06-11 16:30:14 +0000

    add dockerignore

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,4 +7,4 @@ PROJECT_NAME=${1:-my_custom_app}
 docker build --build-arg PROJECT_NAME=${PROJECT_NAME} -t ${PROJECT_NAME} .
 
 # Run the Docker container with the specified project name
-docker run -it --rm -p 8000:8000 ${PROJECT_NAME}
+docker run -it --rm -p 8000:8000 --env-file .env ${PROJECT_NAME}
